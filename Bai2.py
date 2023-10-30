7b3420965058b9f56a022fd5bbca50f5b7e093dc
hoangtung17a1 <149399242+hoangtung17a1@users.noreply.github.com>
2023-10-30 22:39:51 +0700

    ...

---
+++ Bai2.py
@@ -0,0 +1,7 @@
+print("x = 10 , y = 5")
+x = 10
+y = 5
+print("Tổng x + y = "), print(x+y)
+print("Hiệu x - y = "), print(x-y)
+print("Tích x * y = "), print(x*y)
+print("Thương x / y = "), print(x/y)
